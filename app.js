f9317efdfd2d5a58d2be90a8dbf70d5e53804781
razamob <razamob@sheridancollege.ca>
2018-11-20 10:01:44 -0500

    Finished loading image

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    //Hide results
+    document.getElementById('results').style.display = 'none';
 
-function calculateResults(e){
+    //Show loader
+    document.getElementById('loading').style.display = 'block';
+    
+    setTimeout(calculateResults, 2000);
+    e.preventDefault();
+});
+
+function calculateResults(){
 
     //UI Variables
     const amount =  document.getElementById('amount');
@@ -24,14 +33,18 @@ function calculateResults(e){
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly*calculatedPayments) - principal).toFixed(2);
+        document.getElementById('results').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
     }
     else{
         showError('Please check your numbers');
     }
-    e.preventDefault();
+   
 }
 
 function showError(error){
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
     // Create a div
     const errorDiv = document.createElement('div');
 
